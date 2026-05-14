c845da8b1ba16a9db54168afe0bf1a392bb2fb8b
nikBaben <bikita.nabkin@gmail.com>
2026-05-14 15:53:18 +0300

    refactor: export page and component classes via package __init__

---
+++ pages/ip_subcompany/prw/__init__.py
@@ -0,0 +1,12 @@
+from .prw_planning_list_page import (
+    PrwPlanningListPage,
+    PrwPlanningListFilterTitle,
+    PrwPlanningListFilterId, 
+    PrwPlanningListColumn
+)
+from .prw_monthly_planning_page import (
+    PrwMonthlyPlanningPage,
+    PrwMonthlyPlanningFilterTitle,
+    PrwMonthlyPlanningFilterId,
+    PrwMonthlyPlanningColumn
+)
